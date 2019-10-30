97a96b3b709f2fbc6ceca8b2c7930606a56b5148
Pruthvi Patel <pruthvipatel145@gmail.com>
2019-10-30 11:00:51 +0530

    Intial mongodb connection and users route setup.

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+//setting up environment variables
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const db = require("./config/db");
+
+//connectig to the database
+const connectionString =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_CONNECT
+    : process.env.MONGO_CONNECT_TEST;
+
+db(connectionString);
+
+//importing routes
+const usersRoute = require("./routes/endpoints/users");
+
+app.get("/", (req, res) =>
+  res.send("Welcome to the Find A Donor backend API.")
+);
+//setting up routes
+app.use("/users", usersRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+module.exports = connectionString => {
+  mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log(`Connected to the database successfully!`))
+    .catch(err => console.log(`Can't connect to the database!\n`, err));
+};
